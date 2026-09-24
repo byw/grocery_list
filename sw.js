b639abda47259f2d72ed6a9d27438dfd3b6c55ae
Bobby Wang <wangbobby@gmail.com>
2026-09-23 23:06:39 -0700

    feat: add apple-touch-icon PNG for iOS home screen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "grocery-v1";
-const FILES = ["./", "index.html", "app.js", "manifest.webmanifest", "icon.svg"];
+const CACHE = "grocery-v2";
+const FILES = ["./", "index.html", "app.js", "manifest.webmanifest", "icon.svg", "icon.png"];
 self.addEventListener("install", (e) => e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES))));
 self.addEventListener("activate", (e) =>
   e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))))
