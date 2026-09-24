8005c11586f8f40c72b4b9fdb6cdb87eda798cbc
Bobby Wang <wangbobby@gmail.com>
2026-09-23 23:26:08 -0700

    style: drop item count, solid bullets, dimmer completed rows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "grocery-v4";
+const CACHE = "grocery-v5";
 const FILES = ["./", "index.html", "app.js", "manifest.webmanifest", "icon.svg", "icon.png"];
 self.addEventListener("install", (e) => e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES))));
 self.addEventListener("activate", (e) =>
