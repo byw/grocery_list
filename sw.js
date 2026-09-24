5a7598f20f3f3100c8935f5f8ed4b21cece3ca50
Bobby Wang <wangbobby@gmail.com>
2026-09-23 23:28:31 -0700

    style: thicker strike-through on completed items

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "grocery-v5";
+const CACHE = "grocery-v6";
 const FILES = ["./", "index.html", "app.js", "manifest.webmanifest", "icon.svg", "icon.png"];
 self.addEventListener("install", (e) => e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES))));
 self.addEventListener("activate", (e) =>
